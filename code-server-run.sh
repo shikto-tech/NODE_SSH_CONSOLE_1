43db061827158da1b04ee782168541b4ad19eff5
shikto-tech <71165904+shikto-tech@users.noreply.github.com>
2021-09-13 19:59:33 +0600

    Create code-server-run.sh

---
+++ code-server-run.sh
@@ -0,0 +1,4 @@
+wget https://github.com/cdr/code-server/releases/download/v3.11.1/code-server-3.11.1-linux-amd64.tar.gz
+tar -xf code-server-3.11.1-linux-amd64.tar.gz
+cd code-server-3.11.1-linux-amd64
+bash ./code-server
