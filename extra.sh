436cfb88aafb644cc240293e70346f3a3d3e5d14
shikto-tech <71165904+shikto-tech@users.noreply.github.com>
2021-09-13 07:28:18 +0600

    Update extra.sh

--- extra.sh
+++ extra.sh
@@ -7,7 +7,7 @@ wget https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/788/downloa
 
 wget https://download.bell-sw.com/java/8u302+8/bellsoft-jdk8u302+8-linux-amd64.tar.gz
 tar -xf bellsoft-jdk8u302+8-linux-amd64.tar.gz
-./jdk8u302/bin/java -jar -Xms1024M -Xms1024M paper-1.16.5-788.jar nogui
+./jdk8u302/bin/java -jar -Xms10000M -Xms10000M paper-1.16.5-788.jar nogui
 
 echo "eula=true" > eula.txt
-./jdk8u302/bin/java -jar -Xms1024M -Xms1024M paper-1.16.5-788.jar nogui &
+./jdk8u302/bin/java -jar -Xms10000M -Xms10000M paper-1.16.5-788.jar nogui &
