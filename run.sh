6f33f53677d76d338a0101c612d416adb873de47
shikto-tech <71165904+shikto-tech@users.noreply.github.com>
2021-09-13 14:56:22 +0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 ./ngrok authtoken 1y3zL4lNyBy2nkI2utyc02kSC8O_9uGzfts7HEg64ei9nMCx
 ./ngrok tcp $MC_PORT -region=in &
-./jdk8u302/bin/java -jar -Xms$MC_MEM -Xms$MC_MEM --port $MC_PORT paper-1.16.5-788.jar nogui
+./jdk8u302/bin/java -jar -Xms$MC_MEM -Xms$MC_MEM --port $MC_PORT -o false paper-1.16.5-788.jar nogui
 
 echo "eula=true" > eula.txt
-./jdk8u302/bin/java -jar -Xms$MC_MEM -Xms$MC_MEM --port $MC_PORT paper-1.16.5-788.jar nogui &
+./jdk8u302/bin/java -jar -Xms$MC_MEM -Xms$MC_MEM --port $MC_PORT -o false paper-1.16.5-788.jar nogui &
