5cfa287971891b12136163af4888e81e25ee8016
shikto-tech <71165904+shikto-tech@users.noreply.github.com>
2021-09-13 07:21:24 +0600

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,7 @@
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
+unzip ngrok-stable-linux-386.zip
+
+wget https://papermc.io/api/v2/projects/paper/versions/1.16.5/builds/788/downloads/paper-1.16.5-788.jar
+
+wget https://download.bell-sw.com/java/8u302+8/bellsoft-jdk8u302+8-linux-amd64.tar.gz
+tar -xf bellsoft-jdk8u302+8-linux-amd64.tar.gz
